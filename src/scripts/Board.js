aa1127adaa4974df8090272e9dc3e95aaf1051ea
Christian Ruigrok <christian@ruigrok.info>
2016-12-29 11:25:41 +0100

    Cleaned up code

--- src/scripts/Board.js
+++ src/scripts/Board.js
@@ -11,10 +11,10 @@ export default class Board {
 		for (let i = 0; i < config.size * 2; i++) {
 			for (let j = 0; j < config.size; j++) {
 				if (i < config.size - 1) {
-					this.rows[i][j] = new Piece(i, j, 'white', 1)
+					this.rows[i][j] = new Piece(this, i, j, 'white', 1)
 				}
 				if (i > config.size) {
-					this.rows[i][j] = new Piece(i, j, 'black', 1)
+					this.rows[i][j] = new Piece(this, i, j, 'black', 1)
 				}
 			}
 		}
@@ -63,14 +63,14 @@ export default class Board {
 	renderPieces() {
 		let pieces = document.createElement('div')
 		pieces.classList.add('pieces')
+		this.parent.appendChild(pieces)
 		for (let row of this.rows) {
 			for (let piece of row) {
 				if (piece !== undefined) {
-					pieces.appendChild(piece.el)
+					piece.render(pieces)
 				}
 			}
 		}
-		this.parent.appendChild(pieces)
 	}
 
 	init() {
--- src/scripts/Piece.js
+++ src/scripts/Piece.js
@@ -1,25 +1,37 @@
 export default class Piece {
-	constructor(row, col, color, type) {
+	constructor(parent, row, col, color, type) {
+		this.parent = parent
 		this.row = row
 		this.col = col
 		this.color = color
 		this.type = type
 	}
 
-	get el() {
-		let el = this.createPiece()
+	render(parent) {
+		let piece = document.createElement('div')
+		piece.classList.add('piece', this.color)
+		piece.addEventListener('click', () => {
+			console.log(this.possibleMoves())
+		})
+
 		let left = this.col * 8
 		if (!(this.row % 2)) {
 			left += 4
 		}
-		const top = this.row * 4
-		el.style.transform = `translate(${left}em, ${top}em)`
-		return el
+		piece.style.transform = `translate(${left}em, ${this.row * 4}em)`
+
+		parent.appendChild(piece)
 	}
 
-	createPiece() {
-		let piece = document.createElement('div')
-		piece.classList.add('piece', this.color)
-		return piece
+	possibleMoves() {
+		let moves = []
+		if (this.type === 1) {
+			if (this.color === 'black') {
+				if (this.row + 1 === this.parent.rows) {
+					moves.push(this.row[3])
+				}
+			}
+		}
+		return moves
 	}
 }
